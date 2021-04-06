a7ef4879d25bc0b84a3b47437f0305c2284eafee
ericyics <81194049+ericyics@users.noreply.github.com>
2021-04-05 21:47:48 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,17 @@
+/*
+Authors: Eric Y, Ken W
+Date: April 5 2021
+ICS 3U7 Ms S
+Class description: Main class that starts the program
+*/
+
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
-        //creates frame instance
+        //create frame instance
         MyFrame frame= new MyFrame("Tanki Offline");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         frame.setVisible(true);
 
     }
