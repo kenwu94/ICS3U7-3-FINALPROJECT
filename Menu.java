9a129a470ac1295e432f3260fd5d3e31c42d96b6
ericyics <81194049+ericyics@users.noreply.github.com>
2021-04-06 22:10:40 -0400

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,3 +1,12 @@
+  
+/*
+Authors: Eric Y, Ken W
+Date: April 6 2021
+ICS 3U7 Ms S
+Class description: Class for the menu panel
+*/
+
+//imports
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -5,36 +14,37 @@ import javax.swing.*;
 
 
 public class Menu extends JPanel implements ActionListener {
+    //variables
     private JButton play, instructions, exit;
     private JLabel menuBackG;
     Sprites sprites;
     public static TankSelect ts;
 
     public Menu() {
-    	
+    	//initialize sprites instance
         sprites = new Sprites();
         
-        //play button
+        //initialize and set up buttons
         play = new JButton();
         play.setBounds(380, 155, 200, 60);
         play.setText("Play");
         play.setBackground(Color.white);
         play.addActionListener(this);
 
-        //button for instructions
         instructions = new JButton();
         instructions.setBounds(380, 255, 200, 60);
         instructions.setText("Instructions");
         instructions.setBackground(Color.white);
         instructions.addActionListener(this);
 
-        //button to exit
+
         exit = new JButton();
         exit.setBounds(430, 355, 100, 30);
         exit.setText("Exit");
         exit.setBackground(Color.red);
         exit.addActionListener(this);
         
+        //set up panel
         this.add(play);
         this.add(instructions);
         this.add(exit);
@@ -42,22 +52,28 @@ public class Menu extends JPanel implements ActionListener {
         this.setVisible(true);
     }
 
+    //method that handles button presses
     public void actionPerformed(ActionEvent e) {
+        //check if exit button is pressed
         if (e.getSource() == exit) {
             System.exit(0);
         }
+        //check if play button is pressed
         else if(e.getSource() == play) {
+            //initialize and set up a new TankSelect panel
             ts = new TankSelect();
             MyFrame.cont.add(ts, "Tank Selection");
             MyFrame.lay.show(MyFrame.cont, "Tank Selection");
             ts.requestFocusInWindow();
             ts.setFocusable(true);
         }
-
+        //check if instruction button is pressed
         else if (e.getSource() == instructions) {
+            //show instructions panel
             MyFrame.lay.show(MyFrame.cont, "Instructions");
         }
     }
+    //method that draws the background of the menu
     public void paintComponent(Graphics g) {
     	g.drawImage(Sprites.menu,0,0,null);
     }
